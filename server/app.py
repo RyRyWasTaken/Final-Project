b619d59f4ad5341348380fd3f57f4629e566ae95
Rayan Fakereldeen <rfakereldeen@protonmail.com>
2024-04-07 05:28:12 -0700

    Update (2)

--- server/app.py
+++ server/app.py
@@ -2,42 +2,7 @@ from flask import request, jsonify, session
 from config import app, db
 from models import *
 
-@app.route('/signup', methods=['POST'])
-def signup():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
-    
-    new_user = User(username=username, password=password)
-    db.session.add(new_user)
-    db.session.commit()
-    
-    return jsonify({'message': 'User created successfully'}), 201
-
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
-
-    if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
-
-    user = User.query.filter_by(username=username, password=password).first()
-
-    if not user:
-        return jsonify({'message': 'Invalid username or password'}), 401
-
-    session['user_id'] = user.id
-    return jsonify({'message': 'Logged in successfully'}), 200
-
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return jsonify({'message': 'Logged out successfully'}), 200
 
 if __name__ == "__main__":
     with app.app_context():
